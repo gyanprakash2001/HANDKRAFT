07224d4782340f99f665c9756dd8b44e77df509a
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2026-05-23 20:51:48 +0530

    Fix Google Auth client IDs and add phone number scope

--- mobile/app/signup.tsx
+++ mobile/app/signup.tsx
@@ -28,7 +28,7 @@ export default function SignupScreen() {
     iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
     androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
-    scopes: ['openid', 'profile', 'email'],
+    scopes: ['openid', 'profile', 'email', 'https://www.googleapis.com/auth/user.phonenumbers.read'],
     selectAccount: true,
   } as any);
 
