67d8230f379467d7a7c4666b8bf868a942f1c639
gyanprakash2001 <gyanprakashhh2001@gmail.com>
2026-06-27 14:05:22 +0530

    Fix ReferenceError Platform in MessageOptionsModal

--- mobile/components/MessageOptionsModal.tsx
+++ mobile/components/MessageOptionsModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Pressable, Modal, TouchableWithoutFeedback, Clipboard } from 'react-native';
+import { StyleSheet, View, Text, Pressable, Modal, TouchableWithoutFeedback, Clipboard, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as Haptics from 'expo-haptics';
 import { ThemedText } from './themed-text';
